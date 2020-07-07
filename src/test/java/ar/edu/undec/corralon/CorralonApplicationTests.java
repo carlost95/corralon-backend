56157b4488e41c70e3e7c4b06a5e546084663816
carlost95 <carlosanibaltejada@gmail.com>
2020-07-07 03:01:25 -0300

    feat: Creation of proyect

---
+++ src/test/java/ar/edu/undec/corralon/CorralonApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.edu.undec.corralon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CorralonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
