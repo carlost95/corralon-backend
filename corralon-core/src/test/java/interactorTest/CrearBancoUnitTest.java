68f8974d576e623be4147f35833a605a471a0e20
carlost95 <carlosanibaltejada@gmail.com>
2020-07-08 21:16:57 -0300

    feat: Test de caso de uso crear banco

---
+++ corralon-core/src/test/java/interactorTest/CrearBancoUnitTest.java
@@ -0,0 +1,54 @@
+package interactorTest;
+
+import exceptions.BancoExisteException;
+import exceptions.BancoIncompletoException;
+import interactor.CrearBancoUseCase;
+import repository.ICrearBancoRepo;
+import mockito.MockitoExtension;
+import model.Banco;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+
+@ExtendWith(MockitoExtension.class)
+public class CrearBancoUnitTest {
+
+    @Mock
+    ICrearBancoRepo crearBancoRepo;
+
+    @Test
+    public void crearBanco_BancoNoExiste_GuardaCorrectamente() throws BancoIncompletoException, BancoExisteException {
+        Banco banco = Banco.factoryBanco(null, "Banco Rioja", "BR", true);
+        Mockito.when(crearBancoRepo.save(banco)).thenReturn(true);
+        CrearBancoUseCase crearBancoUseCase = new CrearBancoUseCase(crearBancoRepo);
+        boolean resultado = crearBancoUseCase.crearBanco(banco);
+        Assertions.assertTrue(resultado);
+    }
+
+
+    @Test
+    public void crearBancoHabilitacionNula_HabilitacionNull_GuardaCorrectamente() throws BancoIncompletoException, BancoExisteException {
+        Banco banco = Banco.factoryBanco(null, "Banco Rioja", "BR", null);
+        Mockito.when(crearBancoRepo.save(banco)).thenReturn(true);
+        CrearBancoUseCase crearBancoUseCase = new CrearBancoUseCase(crearBancoRepo);
+        boolean resultado = crearBancoUseCase.crearBanco(banco);
+        Assertions.assertTrue(resultado);
+    }
+
+    @Test
+    public void crearBanco_BancoExiste_BancoExisteException() throws BancoIncompletoException {
+        Banco bancoNuevo = Banco.factoryBanco(1, "Banco Rioja", "BR", true);
+        Banco repetido = Banco.factoryBanco(2, "Banco Rioja", "BR", true);
+
+        Mockito.when(crearBancoRepo.findByNombre("Banco Rioja")).thenReturn(repetido);
+        CrearBancoUseCase crearBancoUseCase = new CrearBancoUseCase(crearBancoRepo);
+
+        Assertions.assertThrows(BancoExisteException.class, () ->{
+            crearBancoUseCase.crearBanco(bancoNuevo);
+        });
+
+    }
+
+}
