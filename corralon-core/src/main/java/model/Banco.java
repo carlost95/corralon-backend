0cfeac53d9bb1057e837d7de3df89d90068dca1c
carlost95 <carlosanibaltejada@gmail.com>
2020-07-08 21:16:07 -0300

    feat: Modelo de banco

---
+++ corralon-core/src/main/java/model/Banco.java
@@ -0,0 +1,62 @@
+package model;
+
+import exceptions.BancoIncompletoException;
+
+public class Banco {
+
+    private Integer id;
+    private String nombre;
+    private String abreviatura;
+    private Boolean habilitado;
+
+    public Banco(Integer id, String nombre, String abreviatura, Boolean habilitado) {
+        this.id = id;
+        this.nombre = nombre;
+        this.abreviatura = abreviatura;
+        this.habilitado = habilitado;
+    }
+
+    public static Banco factoryBanco(Integer id, String nombre, String abreviatura, Boolean habilitado) throws BancoIncompletoException {
+
+        if (abreviatura == null  || nombre == null){
+            throw new BancoIncompletoException();
+        }
+
+        if (nombre.length() == 0 || abreviatura.length() == 0 ){
+            throw new BancoIncompletoException();
+        }
+        return new Banco(id, nombre ,abreviatura, habilitado);
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getAbreviatura() {
+        return abreviatura;
+    }
+
+    public void setAbreviatura(String abreviatura) {
+        this.abreviatura = abreviatura;
+    }
+
+    public Boolean getHabilitado() {
+        return habilitado;
+    }
+
+    public void setHabilitado(Boolean habilitado) {
+        this.habilitado = habilitado;
+    }
+}
