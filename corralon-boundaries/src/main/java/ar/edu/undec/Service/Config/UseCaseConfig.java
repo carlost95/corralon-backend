6d75ad6ae69bd6e89cfb58ae4810b0b71fd37b43
carlost95 <carlosanibaltejada@gmail.com>
2020-07-11 08:31:24 -0300

    feat: se agrega bean y repo a la configuracion.

--- corralon-boundaries/src/main/java/ar/edu/undec/Service/Config/UseCaseConfig.java
+++ corralon-boundaries/src/main/java/ar/edu/undec/Service/Config/UseCaseConfig.java
@@ -1,10 +1,13 @@
 package ar.edu.undec.Service.Config;
 
+import input.IUpdateBancoInput;
 import interactor.CrearBancoUseCase;
+import interactor.UpdateBancoUseCase;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import repository.ICrearBancoRepo;
+import repository.IUpdateBancoRepo;
 
 @Configuration
 public class UseCaseConfig {
@@ -12,8 +15,16 @@ public class UseCaseConfig {
     @Autowired
     private ICrearBancoRepo iCrearBancoRepo;
 
+    @Autowired
+    private IUpdateBancoRepo iUpdateBancoRepo;
+
     @Bean
     public CrearBancoUseCase crearBancoUseCase(){
         return new CrearBancoUseCase(iCrearBancoRepo);
+    }
+
+    @Bean
+    public UpdateBancoUseCase updateBancoUseCase() {
+        return new UpdateBancoUseCase(iUpdateBancoRepo);
     }
 }
