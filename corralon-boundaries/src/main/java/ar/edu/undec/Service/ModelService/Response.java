a8e14eb444e2254d1cd7b992d5d3d1fd536c4e3c
carlost95 <carlosanibaltejada@gmail.com>
2020-07-11 08:28:52 -0300

    feat: se crea objeto de respuesta general.

---
+++ corralon-boundaries/src/main/java/ar/edu/undec/Service/ModelService/Response.java
@@ -0,0 +1,35 @@
+package ar.edu.undec.Service.ModelService;
+
+public class Response {
+
+    private Integer status;
+    private String message;
+    private Object data;
+
+    public Response() {
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public Object getData() {
+        return data;
+    }
+
+    public void setData(Object data) {
+        this.data = data;
+    }
+}
