bfcdfab28a974720e4c7f1dd2d361e953d3ed143
carlost95 <carlosanibaltejada@gmail.com>
2020-07-11 08:24:20 -0300

    feat: se testea la actualizacion de un banco

---
+++ corralon-core/src/test/java/interactorTest/UpdateBancoUnitTest.java
@@ -0,0 +1,49 @@
+package interactorTest;
+
+import exceptions.BancoExisteException;
+import exceptions.BancoIncompletoException;
+import exceptions.BancoNoExisteException;
+import interactor.UpdateBancoUseCase;
+import repository.IUpdateBancoRepo;
+import mockito.MockitoExtension;
+import model.Banco;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+public class UpdateBancoUnitTest {
+
+    @Mock
+    IUpdateBancoRepo iUpdateBancoRepo;
+
+
+    @Test
+    public void modificarBanco_BancoExiste_GuardaCorrectamente() throws BancoIncompletoException, BancoNoExisteException {
+        Banco banco = Banco.factoryBanco(1, "Banco Rioja", "BR", true);
+        Banco updated = Banco.factoryBanco(1, "Banco Patagonia", "BP", true);
+
+        when(iUpdateBancoRepo.findById(1)).thenReturn(banco);
+        when(iUpdateBancoRepo.update(updated)).thenReturn(updated);
+
+        UpdateBancoUseCase updateBancoUseCase = new UpdateBancoUseCase(iUpdateBancoRepo);
+
+        Banco result = updateBancoUseCase.update(updated);
+        Assertions.assertNotNull(result);
+    }
+
+    @Test
+    public void modificarBanco_ProblemaBancoExiste_BancoExisteException() throws BancoIncompletoException {
+
+        Banco bancoDatosNuevos = Banco.factoryBanco(1, "Banco Patagonia", "BP", true);
+
+        when(iUpdateBancoRepo.findById(1)).thenReturn(null);
+
+        UpdateBancoUseCase updateBancoUseCase = new UpdateBancoUseCase(iUpdateBancoRepo);
+        Assertions.assertThrows(BancoNoExisteException.class, ()-> updateBancoUseCase.update(bancoDatosNuevos));
+    }
+
+}
