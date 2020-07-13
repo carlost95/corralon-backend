00c1acf40949badee0485bafc554fe59c67aeff0
carlost95 <carlosanibaltejada@gmail.com>
2020-07-13 02:16:07 -0300

    refactor: se agrego fechas de auditoria

--- corralon-boundaries/src/test/resources/banco/beforeFindBancoById.sql
+++ corralon-boundaries/src/test/resources/banco/beforeFindBancoById.sql
@@ -1,3 +1,3 @@
 TRUNCATE banco;
 ALTER TABLE banco AUTO_INCREMENT = 1;
-INSERT INTO banco VALUES (null, "Banco Rioja", "BR", true);
+INSERT INTO banco VALUES (null, "Banco Rioja", "BR", true, null, null);
--- corralon-boundaries/src/test/resources/banco/beforeUpdateBanco.sql
+++ corralon-boundaries/src/test/resources/banco/beforeUpdateBanco.sql
@@ -1,4 +1,4 @@
 TRUNCATE banco;
 ALTER TABLE banco AUTO_INCREMENT = 1;
-INSERT INTO banco VALUES (null, "Banco Rioja", "BR", TRUE);
+INSERT INTO banco VALUES (null, "Banco Rioja", "BR", TRUE, null, null);
 
