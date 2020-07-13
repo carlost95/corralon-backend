1d8ef3a3299f1d15a51b34ff25c4ec0a94b8bd88
carlost95 <carlosanibaltejada@gmail.com>
2020-07-13 02:14:29 -0300

    feat: se agrego auditoria de fechas

---
+++ corralon-core/src/main/java/model/AuditDate.java
@@ -0,0 +1,29 @@
+package model;
+
+
+import java.util.Date;
+
+public class AuditDate {
+
+    private Date createdAt;
+    private Date updatedAt;
+
+    public AuditDate() {
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Date getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(Date updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+}
