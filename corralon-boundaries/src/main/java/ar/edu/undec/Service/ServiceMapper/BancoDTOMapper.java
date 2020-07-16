a3c9a3aa2051efa6616227fe33d53b1beb672876
carlost95 <carlosanibaltejada@gmail.com>
2020-07-15 23:25:01 -0300

    refactor: Se elimino el tratamiento de excepciones.

--- corralon-boundaries/src/main/java/ar/edu/undec/Service/ServiceMapper/BancoDTOMapper.java
+++ corralon-boundaries/src/main/java/ar/edu/undec/Service/ServiceMapper/BancoDTOMapper.java
@@ -5,13 +5,8 @@ import exceptions.BancoIncompletoException;
 import model.Banco;
 
 public class BancoDTOMapper {
-    public Banco mapeoDTOCore(BancoDTO bancoDTO) {
-         try {
+    public Banco mapeoDTOCore(BancoDTO bancoDTO) throws BancoIncompletoException {
              return Banco.factoryBanco(bancoDTO.getId(), bancoDTO.getNombre(), bancoDTO.getAbreviatura(),bancoDTO.isHabilitado());
-         } catch (BancoIncompletoException e) {
-             e.printStackTrace();
-             return null;
-         }
     }
 
     public BancoDTO mapeoCoreDTO(Banco banco) {
