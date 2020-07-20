3aa09f4fedb6f1047ca12a75535d8ed7f77db33e
carlost95 <carlosanibaltejada@gmail.com>
2020-07-20 20:06:59 -0300

    refactor: se cambio instant a date

--- corralon-core/src/main/java/model/AuditDate.java
+++ corralon-core/src/main/java/model/AuditDate.java
@@ -1,29 +1,29 @@
 package model;
 
 
-import java.util.Date;
+import java.time.LocalDateTime;
 
 public class AuditDate {
 
-    private Date createdAt;
-    private Date updatedAt;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
 
     public AuditDate() {
     }
 
-    public Date getCreatedAt() {
+    public LocalDateTime getCreatedAt() {
         return createdAt;
     }
 
-    public void setCreatedAt(Date createdAt) {
+    public void setCreatedAt(LocalDateTime createdAt) {
         this.createdAt = createdAt;
     }
 
-    public Date getUpdatedAt() {
+    public LocalDateTime getUpdatedAt() {
         return updatedAt;
     }
 
-    public void setUpdatedAt(Date updatedAt) {
+    public void setUpdatedAt(LocalDateTime updatedAt) {
         this.updatedAt = updatedAt;
     }
 }
